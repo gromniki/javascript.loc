c7193eb64576c25d70b906ead232524de2494006
Roman Golubev <grom272008@yandex.ru>
2019-03-19 22:25:53 +0300

    life cycle

--- ninja-js/js/script.js
+++ ninja-js/js/script.js
@@ -11,4 +11,24 @@ for (let n = 0; n < maxCount; n++) {  // многократно выполнит
 console.timeEnd('name operation');  // остановить таймер
 
 
-//
+// Общее представление о жизненном цикле
+function addMessage(element, message) {
+    let messageElement = document.createElement('li');
+    messageElement.textContent = message;
+    element.appendChild(messageElement);
+}
+
+let first = document.querySelector('.first-list');
+addMessage(first, 'Page loading');
+
+
+
+document.body.addEventListener('mousemove', function () {
+    let second = document.querySelector('.second-list');
+    addMessage(second, 'Event: mousemove');
+});
+
+document.body.addEventListener('click', function () {
+    let second = document.querySelector('.second-list');
+    addMessage(second, 'Event: click');
+});
