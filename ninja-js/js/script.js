47e1ad3f273bb35bdf42680015137b66ef806585
Roman Golubev <grom272008@yandex.ru>
2019-03-20 20:56:32 +0300

    function start

--- ninja-js/js/script.js
+++ ninja-js/js/script.js
@@ -22,7 +22,6 @@ let first = document.querySelector('.first-list');
 addMessage(first, 'Page loading');
 
 
-
 document.body.addEventListener('mousemove', function () {
     let second = document.querySelector('.second-list');
     addMessage(second, 'Event: mousemove');
@@ -32,3 +31,61 @@ document.body.addEventListener('click', function () {
     let second = document.querySelector('.second-list');
     addMessage(second, 'Event: click');
 });
+
+
+// 3.1 Ряд действий, которые можно выполнять над объектами:
+// • Создавать с помощью литералов
+{
+}
+;
+
+// • Присваивать переменным, элементам массива и свойствам других объектов
+let ninja = {};  // присвоить новый объект переменной
+ninjaArray.push({});  // ввести новый объект в массив
+ninja.data = {}; // присвоить новый объект свойству другого объекта
+
+// • Передавать в качестве аргументов функциям
+function hide(ninja) {
+    ninja.visibility = false;
+}
+
+hide({});  // вновь созданный объект передается функции в качестве аргумента
+
+// • Возвращать в качестве значений из функции
+function returnNewNinja() {
+    return {};  // возвратить новый объект из функции
+}
+
+// • Наделять свойствами, которые можно динамически создавать и присваивать им значения
+let ninjaTwo = {};
+ninjaTwo.name = 'Hanzo';  // создать новое свойство для объекта
+
+// 3.2 Функции в качестве объектов высшего порядка
+// Функции в JavaScript обладают всеми возможностями объектов, а следовательно, их вполне
+// допустимо трактовать как и любые другие объекты в этом языке. Говорят, что функции
+// являются объектами Высшего Порядка, и поэтому их также можно
+
+// • Создавать с помощью литералов
+function ninjaFunction() {
+}
+
+// • Присваивать переменным, элементам массива и свойствам других объектов
+let ninjaFunctionTwo = function () {
+};  // присвоить новую функцию переменной
+ninjaArrayTwo.push(function () {});  // ввести новую функцию в массив
+ninja.data = function () {};  // присвоить новую функцию свойству другого объекта
+
+// • Передавать в качестве аргументов другим функциям
+function call(ninjaFunctionThird) {
+    ninjaFunctionThird()
+}
+call(function () {});  // вновь созданная функция передается в качестве аргумента вызываемой функции
+
+// • Возвращать в качестве значений из других функций
+function returnNewNinjaFunction() {
+    return function () {};  // возвратить новую функцию
+}
+
+// • Наделять свойствами, которые можно динамически создавать и присваивать им значения
+let ninjaFunctionFourth = function () {};
+ninjaFunctionFourth.ninja = 'Hanzo';  // ввести новое свойство в функцию
